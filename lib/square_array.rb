71231ec14ddc3e03923222257db284224a915d5b
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-18 22:38:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,9 @@
 def square_array(array)
   newArr = []
   i = 0 
-  while i < array.length do 
+  while i < array.length 
     num = array[i]
     newNum = i * i
     newArr.push(newNum)
     i++
   end 
-  
-end
